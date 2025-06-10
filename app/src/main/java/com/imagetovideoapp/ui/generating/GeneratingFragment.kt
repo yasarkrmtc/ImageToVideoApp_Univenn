ab776eaddd5706f32999a7d4369da936c7e0d869
Yaşar Kiremitci <yasar.krmtc@gmail.com>
2025-06-10 17:21:52 +0300

    ui start

---
+++ app/src/main/java/com/imagetovideoapp/ui/generating/GeneratingFragment.kt
@@ -0,0 +1,13 @@
+package com.imagetovideoapp.ui.generating
+
+import android.os.Bundle
+import android.view.View
+import com.imagetovideoapp.base.BaseFragment
+import com.imagetovideoapp.databinding.FragmentGeneratingBinding
+
+class GeneratingFragment :
+    BaseFragment<FragmentGeneratingBinding>(FragmentGeneratingBinding::inflate) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/imagetovideoapp/ui/imagetovideo/ImageToVideoFragment.kt
@@ -0,0 +1,13 @@
+package com.imagetovideoapp.ui.imagetovideo
+
+import android.os.Bundle
+import android.view.View
+import com.imagetovideoapp.base.BaseFragment
+import com.imagetovideoapp.databinding.FragmentImageToVideoBinding
+
+class ImageToVideoFragment :
+    BaseFragment<FragmentImageToVideoBinding>(FragmentImageToVideoBinding::inflate) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
