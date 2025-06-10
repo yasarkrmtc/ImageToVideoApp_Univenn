6ce97c7497da0165043e60112717dc20e317ce7f
Yaşar Kiremitci <yasar.krmtc@gmail.com>
2025-06-10 12:09:11 +0300

    .

--- app/src/main/java/com/imagetovideoapp/MainActivity.kt
+++ app/src/main/java/com/imagetovideoapp/MainActivity.kt
@@ -9,8 +9,6 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        
     }
 }
